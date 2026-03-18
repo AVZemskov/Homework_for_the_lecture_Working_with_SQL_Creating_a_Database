1a6fca38380f8b6586eba2e7f12ab63d2fcb3799
A.V.Zemskov <zeman0018@gmail.com>
2026-03-18 14:57:03 +0300

    completion_test1

--- Homework_1_BD_.sql
+++ Homework_1_BD_.sql
@@ -51,3 +51,63 @@ CREATE TABLE compilation_tracks (
     FOREIGN KEY (compilation_id) REFERENCES compilations(compilation_id),
     FOREIGN KEY (track_id) REFERENCES track(treck_id)
 );
+
+-- ЗАПОЛНЕНИЕ --
+
+-- 1. Заполнение исполнителей
+INSERT INTO performers (name_executor) VALUES 
+('Zefirych'),
+('Zivert'),
+('Pankevich'),
+('Бастон'),
+('Бетхов');
+
+-- 2. Заполнение жанров
+INSERT INTO musical_genres (name_genres) VALUES 
+('Рок'),
+('Поп'),
+('Реп'),
+('Класическая');
+
+-- 3. Заполнение альбомов
+INSERT INTO albums (alboms_name, year_release) VALUES 
+('Linkin Garden', 2017),
+('Tralee-Woolie', 2019),
+('Бастон/AND/Zivert', 2013),
+('Баста за 40', 2020),
+('Dreams-26', 2020);
+
+-- 4. Заполнение сборников
+INSERT INTO compilations (compilation_name, year_release) VALUES 
+('Рок хиты 2017', 2018),
+('Зажигательная музыка', 2020),
+('Реп-баллады', 2021),
+('Хиты десятилетия', 2022),
+('Авто сборник', 2023);
+
+-- 5. Заполнение связей исполнителей с жанрами
+INSERT INTO performers_genres (performer_id, genre_id) VALUES 
+(1, 1), (1, 3), (2, 2), (2, 3), (3, 3), (4, 4), (4, 2), (5, 2), (5, 3);
+
+-- 6. Заполнение связей исполнителей с альбомами
+INSERT INTO performers_albums (performer_id, album_id) VALUES 
+(1, 1), (2, 2), (3, 3), (4, 4), (5, 5);
+
+-- 7. Заполнение треков
+INSERT INTO track (treck_name, time, alboms_id) VALUES 
+('Believer', '00:03:24', 1),
+('Thunder', '00:03:07', 1),
+('Life', '00:03:08', 2),
+('Get Lucky', '00:04:08', 3),
+('Сансара', '00:04:35', 4),
+('Blinding Lights', '00:03:20', 5),
+('Whatever It Takes', '00:03:21', 1),
+('Fly', '00:03:15', 2);
+
+-- 8. Заполнение связей сборников с треками
+INSERT INTO compilation_tracks (compilation_id, track_id) VALUES 
+(1, 1), (1, 2), (1, 6),
+(2, 3), (2, 4), (2, 8),
+(3, 1), (3, 7),
+(4, 5), (4, 6),
+(5, 2), (5, 4), (5, 5), (5, 6);
