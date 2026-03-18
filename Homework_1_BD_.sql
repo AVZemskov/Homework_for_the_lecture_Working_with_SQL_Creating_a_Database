1af7cb44afda89bfcdec9288ab90617890bd27ed
A.V.Zemskov <zeman0018@gmail.com>
2026-03-18 15:48:41 +0300

    SELECT_requests

--- Homework_1_BD_.sql
+++ Homework_1_BD_.sql
@@ -102,7 +102,8 @@ INSERT INTO track (treck_name, time, alboms_id) VALUES
 ('Сансара', '00:04:35', 4),
 ('Blinding Lights', '00:03:20', 5),
 ('Whatever It Takes', '00:03:21', 1),
-('Fly', '00:03:15', 2);
+('Fly', '00:03:15', 2),
+('my treck', '00:03:00', 1);
 
 -- 8. Заполнение связей сборников с треками
 INSERT INTO compilation_tracks (compilation_id, track_id) VALUES 
@@ -111,3 +112,31 @@ INSERT INTO compilation_tracks (compilation_id, track_id) VALUES
 (3, 1), (3, 7),
 (4, 5), (4, 6),
 (5, 2), (5, 4), (5, 5), (5, 6);
+
+-- Задание №2 SELECT-запросы --
+
+-- 1. Название и продолжительность самого длительного трека
+SELECT treck_name, time 
+FROM track 
+ORDER BY time DESC 
+LIMIT 1;
+
+-- 2. Название треков, продолжительность которых не менее 3,5 мин.
+SELECT treck_name 
+FROM track 
+WHERE time >= INTERVAL '3 minutes 30 seconds';
+
+-- 3. Название сборников, вышедших в период с 2018 по 2020г. включительно
+SELECT compilation_name 
+FROM compilations 
+WHERE year_release BETWEEN 2018 AND 2020;
+
+-- 4. Исполнители чье имя состоит из одного слова
+SELECT name_executor 
+FROM performers 
+WHERE name_executor NOT LIKE '% %';
+
+-- 5. Название треков, которые содержат слово "мой" или "my"
+SELECT treck_name 
+FROM track 
+WHERE treck_name ILIKE '%my%' OR treck_name ILIKE '%мой%';
